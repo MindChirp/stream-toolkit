ac8b8e3788e4493dc74da381e6e29063677fd979
mindchirp <frikk44@gmail.com>
2025-10-12 03:29:48 +0200

    Changed to heimdall telemetry handler

--- src/server/api/singleton.ts
+++ src/server/api/singleton.ts
@@ -1,9 +1,8 @@
 import { ServerEventHandlerHeimdall } from "@/lib/telemetry/telemetry-client-heimdall";
 import EventEmitter from "events";
 import { Overlay } from "./types/overlay";
-import { ServerEventHandlerRetrofit } from "@/lib/telemetry/telemetry-client-retrofit";
 
-type Handler = ServerEventHandlerRetrofit;
+type Handler = ServerEventHandlerHeimdall;
 
 const g = globalThis as {
   __ee?: EventEmitter;
@@ -23,5 +22,5 @@ export const overlay: Overlay = g.__overlay ?? new Overlay();
 g.__overlay ??= overlay;
 
 export const serverListener: Handler =
-  g.__serverListener ?? new ServerEventHandlerRetrofit();
+  g.__serverListener ?? new ServerEventHandlerHeimdall();
 g.__serverListener ??= serverListener;
